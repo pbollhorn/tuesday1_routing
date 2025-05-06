8112a78114812a1f9d1a97625aabca228dcef97a
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-06 14:39:26 +0200

    working example

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", backgroundColor: "#eee" }}>
+      <Link to="/" style={{ marginRight: "1rem" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ marginRight: "1rem" }}>
+        About
+      </Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -22,16 +22,42 @@
 //   </BrowserRouter>
 // );
 
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+// import * as React from "react";
+// import * as ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import "./index.css";
+// import Root from "./routes/root";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root";
+import App from "./App";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ],
   },
 ]);
 
